405907389c1cd55c20397629691282d2d331adf4
Jan <jan.weber123@googlemail.com>
2019-12-29 17:37:38 +0100

    erster aufschlag

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:expandable_bottom_bar/expandable_bottom_bar.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MaterialApp(home: AgilPoker()));
 
-class MyApp extends StatelessWidget {
+const double expandedHeight = 550;
+
+var wert = ['0', '0,5', '1', '2', '3', '5', '8', '13', '20', '40', '100', 'start'];
+
+class AgilPoker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Column(
+      home: DefaultBottomBarController(
+        dragLength: expandedHeight,
+        snap: true,
+        child: Page(),
+      ),
+    );
+  }
+}
+
+class Page extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+
+      //Set to true for bottom appbar overlap body content
+      extendBody: true,
+
+      // appBar: AppBar(
+      //   title: Text("Panel Showcase"),
+      //   backgroundColor: Colors.black,
+      // ),
+      body: SafeArea(
+        child: Center(
+          child: Text(
+            wert[10],
+            style: TextStyle(
+              fontFamily: 'Monoton-Regular',
+              fontSize: 160.0,
+              color: Colors.white,
+              // fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ),
+
+      // Lets use docked FAB for handling state of sheet
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: GestureDetector(
+        // Set onVerticalDrag event to drag handlers of controller for swipe effect
+        onVerticalDragUpdate: DefaultBottomBarController.of(context).onDrag,
+        onVerticalDragEnd: DefaultBottomBarController.of(context).onDragEnd,
+        child: FloatingActionButton.extended(
+          icon: Icon(Icons.filter_9_plus),
+          label: Text("wähle"),
+          elevation: 2,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+
+          //Set onPressed event to swap state of bottom bar
+          onPressed: () => DefaultBottomBarController.of(context).swap(),
+        ),
+      ),
+
+      // Actual expandable bottom bar
+      bottomNavigationBar: BottomExpandableAppBar(
+        expandedHeight: expandedHeight,
+        horizontalMargin: 2,
+        shape: AutomaticNotchedShape(
+            RoundedRectangleBorder(), StadiumBorder(side: BorderSide())),
+        expandedBackColor: Colors.grey[900],
+        expandedBody: Center(
+          child: Text("Hello world!"),
+        ),
+        bottomAppBarBody: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
             children: <Widget>[
-              Text('Hi'),
+              Expanded(
+                child: Text(
+                  "Tets",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Spacer(
+                flex: 2,
+              ),
+              Expanded(
+                child: Text(
+                  "Stet",
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ],
           ),
         ),
